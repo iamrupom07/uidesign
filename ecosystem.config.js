87280740e9aa0d1a89b6f52f439f0895522f3413
Rupom <saidurrahmanrupom@gmail.com>
2026-08-02 23:31:31 +0600

    vps

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps: [
+    {
+      name: 'macprotec-api',
+      cwd: './apps/api',
+      script: 'dist/server.js',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 5000,
+      },
+    },
+    {
+      name: 'macprotec-web',
+      cwd: './apps/web',
+      script: 'node_modules/next/dist/bin/next',
+      args: 'start -p 3000',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3000,
+      },
+    },
+  ],
+};
